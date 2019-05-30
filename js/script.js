6f771ffd6094121a08de50588037f6b2a4f52c45
jhkiehna <jhkiehna@gmail.com>
2019-05-30 10:20:39 -0400

    added comment from sindresorhus/refined-github repo

--- js/script.js
+++ js/script.js
@@ -130,6 +130,14 @@ var displaySearchField = () => {
   searchButton.parentNode.removeChild(searchButton);
 };
 
+/*
+ * When navigating back and forth in history, GitHub will preserve the DOM changes;
+ * This means that the old features will still be on the page and don't need to re-run.
+ * For this reason `onAjaxedPages` will only call its callback when a *new* page is loaded.
+ *
+ * Alternatively, use `onAjaxedPagesRaw` if your callback needs to be called at every page
+ * change (e.g. to "unmount" a feature / listener) regardless of of *newness* of the page.
+ */
 document.addEventListener("pjax:end", () => {
   let commentTextArea = document.querySelector("#new_comment_field");
 
