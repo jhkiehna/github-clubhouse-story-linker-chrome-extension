240725feb28d4c003b612af69b20af5653ca048f
jhkiehna <jhkiehna@gmail.com>
2019-05-31 16:17:59 -0400

    cleaning up popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,19 @@
-let apiTokenField = document.querySelector("#api-token-field");
+var setToken = event => {
+  if (event.keyCode != 13) {
+    return;
+  }
 
-apiTokenField.addEventListener(
-  "keypress",
-  event => {
-    if (event.keyCode != 13) {
-      return;
+  chrome.runtime.sendMessage(
+    { contentScriptQuery: "setToken", token: event.target.value },
+    response => {
+      document.querySelector("#api-message").innerText = response;
     }
+  );
+};
 
-    chrome.runtime.sendMessage(
-      { contentScriptQuery: "setToken", token: event.target.value },
-      response => {
-        document.querySelector("#api-message").innerText = response;
-      }
-    );
-  },
-  false
-);
+document
+  .querySelector("#api-token-field")
+  .addEventListener("keypress", setToken, false);
 
 chrome.runtime.sendMessage({ contentScriptQuery: "checkToken" }, response => {
   document.querySelector("#api-message").innerText = response;
